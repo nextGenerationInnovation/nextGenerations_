745e935d3c9216a4c1e5053a3e62e8c743fa56e6
owinoalfred <ogzialfred@gmail.com>
2024-10-16 13:44:46 +0300

    notifications

---
+++ utills/notifications.py
@@ -0,0 +1,10 @@
+﻿def generate_alert(data):
+    alerts = []
+    for index, row in data.iterrows():
+        if row['Risk_Level'] == "Medium":
+            alerts.append(f"⚠️ {row['Contaminant']} levels are elevated. Increased monitoring needed.")
+        elif row['Risk_Level'] == "High":
+            alerts.append(f"🔶 {row['Contaminant']} levels require immediate attention.")
+        elif row['Risk_Level'] == "Critical":
+            alerts.append(f"🚨 {row['Contaminant']} levels are critical! Emergency response required.")
+    return alerts
