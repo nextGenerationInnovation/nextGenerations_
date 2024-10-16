c615cd28a25879700afa8806b1baad94def89cde
owinoalfred <ogzialfred@gmail.com>
2024-10-16 13:43:34 +0300

    threshholds added

---
+++ utills/threshholds.py
@@ -0,0 +1,13 @@
+thresholds = {
+    'E. coli': {'low': 10, 'medium': 100, 'high': 1000},
+    'Coliform': {'low': 10, 'medium': 100, 'high': 1000},
+    'Salmonella': {'low': 0, 'medium': 10, 'high': 100},
+    'Lead': {'low': 0.01, 'medium': 0.05, 'high': 0.1},
+    'Arsenic': {'low': 0.01, 'medium': 0.05, 'high': 0.1},
+    'Mercury': {'low': 0.001, 'medium': 0.005, 'high': 0.01},
+    'Cadmium': {'low': 0.003, 'medium': 0.01, 'high': 0.05},
+    'Chromium': {'low': 0.05, 'medium': 0.1, 'high': 0.5},
+    'Nitrates': {'low': 20, 'medium': 50, 'high': 100},
+    'Fluoride': {'low': 1.0, 'medium': 2.0, 'high': 4.0},
+    'Chlorine': {'low': 2, 'medium': 5, 'high': 10}
+}
