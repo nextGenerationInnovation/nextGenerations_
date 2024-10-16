261eafaa7a991371fc582ab04e065fdb51b359a6
owinoalfred <ogzialfred@gmail.com>
2024-10-16 13:42:26 +0300

    visual added

---
+++ utills/visualization.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def plot_trend(data):
+    st.subheader("Contaminant Levels Over Time")
+    fig, ax = plt.subplots(figsize=(10, 5))
+    sns.lineplot(x='Time', y='Level', hue='Contaminant', data=data, ax=ax)
+    st.pyplot(fig)
+
+def plot_risk_levels(data):
+    st.subheader("Risk Levels")
+    fig, ax = plt.subplots(figsize=(10, 5))
+    sns.barplot(x='Contaminant', y='Level', hue='Risk_Level', data=data, ax=ax)
+    st.pyplot(fig)
